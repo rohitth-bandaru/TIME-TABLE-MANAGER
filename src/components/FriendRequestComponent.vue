ff7f163c6f5d1b94f83dc522439590726aaaaad5
Rohitth Bandaru <f20190541@hyderabad.bits-pilani.ac.in>
2023-09-14 10:38:17 +0530

    added friendrequest component

---
+++ src/components/FriendRequestComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>
+            {{ user }}
+        </div>
+        <button>
+            accept
+        </button>
+        <button>
+            reject
+        </button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      user: String,
+      
+    },
+    methods: {
+      handleClick() {
+        this.$emit("selected");
+      },
+    },
+  };
+  </script>
+  
--- src/veiws/FriendsPage.vue
+++ src/veiws/FriendsPage.vue
@@ -1,33 +1,40 @@
 <template>
   <div>Friends page</div>
-  <input
-    class="rounded-lg border-2 border-black"
-    placeholder="enter email address"
-    v-model="emailaddress"
-  />
-  <button class="rounded-lg bg-sky-500 w-24 ml-3" @click="Submit">
-    Submit
-  </button>
-  <div>{{ error }}</div>
+  <div>
+    <input
+      class="rounded-lg border-2 border-black"
+      placeholder="enter email address"
+      v-model="emailaddress"
+    />
+    <button class="rounded-lg bg-sky-500 w-24 ml-3" @click="Submit">
+      Submit
+    </button>
+    <div>{{ error }}</div>
+    <div :disabled="emailVerified">
+      <span>{{ VerificationMessage }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 import { verifyEmail } from "@/timetable.js";
+import FRComponent from "@/components/FriendRequestComponent.vue";
 export default {
   data() {
     return {
       emailaddress: null,
       error: null,
+      VerificationMessage: "initial",
+      emailVerified: true,
     };
   },
   computed: {
     ...mapGetters(["getUserData"]),
   },
-  methods: {
-    async Submit() {
-      verifyEmail(this.emailaddress).then((response) => console.log(response));
-    },
+  methods: {},
+  components: {
+    FRComponent,
   },
 };
 </script>
